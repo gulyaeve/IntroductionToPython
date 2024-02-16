c53144110fe6c26bc9a6db2bceadd95c109dcdaa
eugene <gulayeve@icloud.com>
2024-02-16 20:55:01 +0300

    Добавлен 6 урок

--- lesson3/module_class_2.py
+++ lesson3/module_class_2.py
@@ -5,5 +5,12 @@ class Database:
     def make_connect(cls, srv: str, port: int):
         print(f"Connected {srv}, {port}")
 
+    @staticmethod
+    def some_func(a):
+        return a * 2
+
 
 Database.make_connect(srv="Yandex.ru", port=22)
+
+x = Database()
+x.some_func(2)
